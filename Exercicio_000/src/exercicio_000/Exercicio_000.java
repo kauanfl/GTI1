6f336bedc4da46178c776b2e7c9b1b8ca8e5e002
Kauan Ferreira Lemos <134456913+kauanfl@users.noreply.github.com>
2024-02-20 10:56:09 -0300

    Add files via upload

---
+++ Exercicio_000/src/exercicio_000/Exercicio_000.java
@@ -0,0 +1,13 @@
+package exercicio_000;
+
+public class Exercicio_000 {
+    
+    public static void main(String[] args) {
+      System.out.println("Kauan Ferreira Lemos");
+            System.out.print("Kauan ");
+                  System.out.print("Ferreira ");
+                        System.out.println("Lemos");
+        // TODO code application logic here
+    }
+    
+}
