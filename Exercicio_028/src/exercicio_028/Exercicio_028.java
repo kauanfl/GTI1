1a5959b516df8bb1abed66ce44cd3602e028914b
Kauan Ferreira Lemos <134456913+kauanfl@users.noreply.github.com>
2024-02-27 10:47:09 -0300

    Add files via upload

---
+++ Exercicio_028/src/exercicio_028/Exercicio_028.java
@@ -0,0 +1,12 @@
+package exercicio_028;
+
+public class Exercicio_028 {
+
+    public static void main(String[] args) {
+        //nome do desenvolvedor
+        System.out.println("Kauan Ferreira Lemos");
+        //mensagem de texto
+        System.out.println("Estudo na FATEC Barueri curso de GTI Gestão da Tecnologia da Informação");
+    }
+    
+}
