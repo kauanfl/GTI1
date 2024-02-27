fc645a9e4689bccb712f94e4acfc92dc83fb97ce
Kauan Ferreira Lemos <134456913+kauanfl@users.noreply.github.com>
2024-02-27 11:21:18 -0300

    Add files via upload

---
+++ Exercicio_029/src/exercicio_029/Exercicio_029.java
@@ -0,0 +1,11 @@
+package exercicio_029;
+
+public class Exercicio_029 {
+
+    public static void main(String[] args) {
+        //imprimir seu nome na tela
+       System.out.println("Kauan Ferreira Lemos");
+                                         
+    }
+    
+}
