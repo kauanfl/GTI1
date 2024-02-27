4335dd1ef275535b8aaea610b96231f4b6ab1f2d
Kauan Ferreira Lemos <134456913+kauanfl@users.noreply.github.com>
2024-02-27 11:41:44 -0300

    Add files via upload

---
+++ Exercicio_030/src/exercicio_030/Exercicio_030.java
@@ -0,0 +1,20 @@
+package exercicio_030;
+
+public class Exercicio_030 {
+    
+    public static void main(String[] args) {
+//Criar um algoritmo que imprima o produto entre 67 e 45. Resposta: 3015        
+       // variaveis 
+       int x;
+       int y;
+       int z;
+//entrada de dados
+x = 45;
+y = 67;
+//processamento
+z = x * y;
+//saida de dados
+System.out.println("A conta 67 * 45 = " + z);
+    }
+    
+}
