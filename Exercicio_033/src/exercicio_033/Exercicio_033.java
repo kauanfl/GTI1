da67138bf62f2ea4cc6e467f0eac36e46583ebaa
Kauan Ferreira Lemos <134456913+kauanfl@users.noreply.github.com>
2024-03-03 20:58:55 -0300

    Add files via upload

---
+++ Exercicio_033/src/exercicio_033/Exercicio_033.java
@@ -0,0 +1,26 @@
+package exercicio_033;
+
+//Importa biblioteca 
+import java.util.Scanner;
+
+public class Exercicio_033 {
+
+    public static void main(String[] args) {
+        //Ler dois numeros inteiros e imprimi-los
+        
+        //Variaveis
+        Scanner teclado = new Scanner(System.in);
+        int numb1, numb2;
+        
+        //Entrada de dados
+        System.out.println("Informe o primeiro numero: ");
+        numb1 = teclado.nextInt();
+        
+        System.out.println("Informe o segundo numero: ");
+        numb2 = teclado.nextInt();
+        
+        //Saida de dados
+        System.out.println("Os numeros informados foram: "+ numb1 + " e "+ numb2);
+    }
+    
+}
