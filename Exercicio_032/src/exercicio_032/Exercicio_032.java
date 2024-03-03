6afdb83cf66174d362935ac1bead120166a562b9
Kauan Ferreira Lemos <134456913+kauanfl@users.noreply.github.com>
2024-03-03 20:42:38 -0300

    Add files via upload

---
+++ Exercicio_032/src/exercicio_032/Exercicio_032.java
@@ -0,0 +1,21 @@
+package exercicio_032;
+
+import java.util.Scanner;
+
+public class Exercicio_032 {
+
+    public static void main(String[] args) {
+        //Ler um numero inteiro e imprimi-lo
+        
+        //Variaveis
+       Scanner teclado = new Scanner(System.in);
+       int numb;
+       
+       System.out.println("Informe o numero: ");
+        numb = teclado.nextInt();
+        
+        System.out.println("O numero informado foi: "+ numb);
+       
+    }
+    
+}
