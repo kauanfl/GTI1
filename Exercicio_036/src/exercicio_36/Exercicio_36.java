6d6c7635a80a845b28d7a4c9d49b79333da6fe9c
Kauan Ferreira Lemos <134456913+kauanfl@users.noreply.github.com>
2024-03-04 12:42:23 -0300

    Add files via upload

---
+++ Exercicio_036/src/exercicio_36/Exercicio_36.java
@@ -0,0 +1,34 @@
+package exercicio_36;
+
+import java.util.Scanner;
+        
+public class Exercicio_36 {
+
+    public static void main(String[] args) {
+        /*Ler dois numeros inteiros e imprimir a soma.
+       Antes do resultado devera aparecer a mensagem: "Soma"*/
+        
+        //Variaveis
+        Scanner teclado = new Scanner(System.in);
+        int numb1;
+        int numb2;
+        int result;
+        
+       //Entrada de dados
+       System.out.print("Informe o primeiro numero: ");
+       numb1 = teclado.nextInt();
+       
+        System.out.print("Informe o segundo numero: ");
+       numb2 = teclado.nextInt();
+       
+       //Processamento 
+       result = numb1 + numb2;
+       
+       //Saida de dados
+       System.out.println("O resultado da soma é: "+ result);
+                
+       
+       
+    }
+    
+}
