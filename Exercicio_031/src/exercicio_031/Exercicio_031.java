9849205bda9f1721ba74ee22eda61cff8a5ad5e8
Kauan Ferreira Lemos <134456913+kauanfl@users.noreply.github.com>
2024-03-05 08:32:26 -0300

    Add files via upload

---
+++ Exercicio_031/src/exercicio_031/Exercicio_031.java
@@ -0,0 +1,32 @@
+package exercicio_031;
+
+public class Exercicio_031 {
+
+    public static void main(String[] args) {
+        //Criar um algoritmo que imprima a media aritmedica entre os numeros 5, 6 e 7
+        
+        //Variaveis
+        double numbX;
+        double numbY;
+        double numbZ;
+        double result;
+        
+        
+        //Entrada de dados
+       numbX = 5;
+       numbY = 6;
+       numbZ = 7;
+        
+        //Operacao
+        result = 0;
+        result = (numbX + numbY + numbZ) / 3.0;
+        
+        
+       //Saida de dados
+       System.out.println("A media aritimetica da soma de 5, 6 e 7 é: " + result);
+       
+        
+        
+    }
+    
+}
