709cc9b11a9e2983a7ec5ce0a333a853c313776b
Kauan Ferreira Lemos <134456913+kauanfl@users.noreply.github.com>
2024-04-29 22:11:10 -0300

    Add files via upload

---
+++ Exercicio_174/src/exercicio_174/Exercicio_174.java
@@ -0,0 +1,17 @@
+package exercicio_174;
+
+public class Exercicio_174 {
+
+    public static void main(String[] args) {
+        //Imprimir todos os numeros de 1 a 100
+        
+        //Variaveis 
+        int i;
+        
+        //Processamento
+        for (i=1; i<= 100; i++) {
+           System.out.println(i);
+        }
+    }
+    
+}
