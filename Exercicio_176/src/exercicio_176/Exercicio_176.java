2a3e47a832d3fc2f8f1d5cd088e0ba964f1ae420
Kauan Ferreira Lemos <134456913+kauanfl@users.noreply.github.com>
2024-04-29 22:14:12 -0300

    Add files via upload

---
+++ Exercicio_176/src/exercicio_176/Exercicio_176.java
@@ -0,0 +1,22 @@
+package exercicio_176;
+
+public class Exercicio_176 {
+
+    public static void main(String[] args) {
+        //Imprimir todos os 100 primeiros numeros pares
+        
+        //Variaveis
+        int i;
+        
+        //Processamento
+        
+        for (i=2; i<= 100; i++) {
+            if (i % 2 == 0) {
+        System.out.println(i);
+            }
+       
+        }
+    }
+    
+}
+    
