a6f9ec0fa90c52279bb8971791d7419764e22525
Kauan Ferreira Lemos <134456913+kauanfl@users.noreply.github.com>
2024-04-29 22:16:54 -0300

    Add files via upload

---
+++ Exercicio_181/src/exercicio_181/Exercicio_181.java
@@ -0,0 +1,23 @@
+package exercicio_181;
+
+public class Exercicio_181 {
+
+    public static void main(String[] args) {
+        //Criar um algoritmo que imprima todos os numeros de 1 a 100 e a soma deles
+        
+        //Variaveis 
+        int i;
+        int soma = 0;
+     
+      
+      //Processamento e saida
+      for (i = 1; i <= 100; i++) {
+        soma += i;
+        System.out.println(soma);
+        
+      }
+      
+      
+    }
+    
+}
