c330fd97e7c3b6fd8258f511fadcfde217d24659
Kauan Ferreira Lemos <134456913+kauanfl@users.noreply.github.com>
2024-04-29 22:13:01 -0300

    Add files via upload

---
+++ Exercicio_175/src/exercicio_175/Exercicio_175.java
@@ -0,0 +1,17 @@
+package exercicio_175;
+
+public class Exercicio_175 {
+
+    public static void main(String[] args) {
+        //Imprimir todos os numeros de 100 a 1
+        
+        //Variaveis
+        int i;
+        
+        //Processamento 
+        for (i = 100; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+    
+}
