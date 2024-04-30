91dee025781c39fd9f866573c191425f101e47a8
Kauan Ferreira Lemos <134456913+kauanfl@users.noreply.github.com>
2024-04-29 22:15:58 -0300

    Add files via upload

---
+++ Exercicio_178/src/exercicio_178/Exercicio_178.java
@@ -0,0 +1,19 @@
+package exercicio_178;
+
+public class Exercicio_178 {
+
+    public static void main(String[] args) {
+        //Imprimir o quadrado dos numeros de 1 ate 20 
+        
+        //Variaveis
+        int i;
+        int quadrado;
+        
+        //Processamento 
+        for (i = 1; i <= 20; i++){
+            quadrado = i*i;
+            System.out.println(quadrado);
+        }
+    }
+     
+}
